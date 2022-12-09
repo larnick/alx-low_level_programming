4e7c93c9a19dfd7226513040ab4bcf9378c1966a
larnick <nixonkoech5@gmail.com>
2022-12-09 04:27:57 -0800

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,26 +5,29 @@
  */
 int main(void)
 {
-		int i, e;
-			i = 48;
-				e = 48;
-					while (e < 58)
-					{
-						i = 48;
-					while (i < 58)
-					{
-						putchar(e);
-						putchar(i);
-		if (i == 57 && e == 57)
+	int i, e;
+	i = 48;
+	e = 48;
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
 		{
-			break;
-		}
-			putchar(',');
-			putchar(' ');
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i==57 && e ==56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
 			i++;
-					}
-		e++;
-					}
-						putchar('\n');
-						return (0);
+		}
+		e++;					}
+	}
+	putchar('\n');
+	return (0);
 }
