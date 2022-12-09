97cb225f462f80830f12dac3e4faa6a619e758be
larnick <nixonkoech5@gmail.com>
2022-12-09 03:09:05 -0800

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-	   for (i = 0; i < 26; i++)
-   {
-	putchar(alp[i]);
-   }
-        putchar('\n');
+			int i;
+					for (i = 0; i < 26; i++)
+					{
+						putchar(alp[i]);
+					}
+					putchar('\n');
 	return (0);
 }
