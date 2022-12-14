727a47db9d2ee50c0ce562b990f220843a374854
larnick <nixonkoech5@gmail.com>
2022-12-13 22:56:04 -0800

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,23 +13,23 @@ void times_table(void)
 			_putchar('0');
 			_putchar(',');
 			_putchar(' ');
-				for (cone = 1; cone <= 9; cone++)						{
-					d = (rone * cone);
-					if ((d / 10) > 0)
-					{
-						_putchar((d / 10) + '0');
-					}
-					else
-					{
-						_putchar(' ');
-					}
-					_putchar((d % 10) + '0');
-					if (cone < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+			for (cone = 1; cone <= 9; cone++)
+			{
+				d = (rone * cone);
+				if ((d / 10) > 0)
+				{
+					_putchar((d / 10) + '0');						}
+				else
+				{
+					_putchar(' ');
 				}
-				_putchar('\n');
+					_putchar((d % 10) + '0');
+				if (cone < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
 }
