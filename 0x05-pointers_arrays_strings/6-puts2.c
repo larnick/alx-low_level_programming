6aa65b84001f3e061ca59d34a2d4770b12641274
larnick <nixonkoech5@gmail.com>
2022-12-19 11:56:53 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,17 @@
  */
 void puts2(char *str)
 {
-	int count = 0;
-		while (count >= 0)
+	int i;
+	int j = 0;
+		while (str[j] != '\0')
 		{
-			if (str[count] == '\0')
-			{
-				_putchar('\n');
-				break;
-			}
-			if (count % 2 == 0)
-				_putchar(str[count]);
-				count++;
+			j++;
 		}
+		for (i = 0; i < j; i += 2)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
 }
+
 
